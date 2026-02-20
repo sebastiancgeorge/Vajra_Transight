a9f008f3912afe4095e01acf4be510f531e0be47
Sebastian George <sebastiangeorge004@gmail.com>
2026-02-20 21:50:58 +0000

    make the upload audio function accept all common file formats like mpeg

--- src/components/dashboard/analysis-form.tsx
+++ src/components/dashboard/analysis-form.tsx
@@ -92,7 +92,7 @@ export function AnalysisForm({
             ref={fileInputRef}
             onChange={handleFileChange}
             className="hidden"
-            accept="audio/*"
+            accept="audio/*,video/mp4,video/mpeg,video/quicktime"
             disabled={isTranscribing}
           />
         </div>
